0a708d46c302b7cbb8ceccbd9b470ff2ce3d05c0
romain <romain.feron@evobio.eu>
2016-05-18 12:53:59 +0200

    .

--- src/main/master.cpp
+++ src/main/master.cpp
@@ -133,9 +133,11 @@ void Master::processBatches(uint i){
         it = std::next(it, this->nextBatchStart);
         this->nextBatchStart += this->batchSize;
 
-        std::cout << "NextBatchStart : " << this->nextBatchStart << " | ReadsEnd : " << this->reads.end()->first << " | End : " << end->first << std::endl;
+        std::cout << "NextBatchStart : " << this->nextBatchStart << std::endl;
+        std::cout << "ReadsEnd : " << (this->reads.end()--)->first << std::endl;
+        std::cout << "End : " << end->first << std::endl;
 
-        if (this->nextBatchStart > this->reads.end()->first){
+        if (this->nextBatchStart > (this->reads.end()--)->first){
 
             end = this->reads.end();
 
